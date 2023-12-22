afb6f7d30fcb64cd528d79825a49fde7e42e0bcf
Ilya <ilyaushenin@gmail.com>
2023-12-22 18:53:25 +0300

    StatusBar transparent

--- app/src/main/java/com/birdushenin/newssphere/presentation/MainFragment.kt
+++ app/src/main/java/com/birdushenin/newssphere/presentation/MainFragment.kt
@@ -1,14 +1,19 @@
 package com.birdushenin.newssphere.presentation
 
 import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
@@ -29,6 +34,12 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window: Window = requireActivity().window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.blue)
+        }
 
         val view = inflater.inflate(R.layout.fragment_main, container, false)
         val viewPager: ViewPager2 = view.findViewById(R.id.viewPager)
--- app/src/main/java/com/birdushenin/newssphere/presentation/NewsWindowFragment.kt
+++ app/src/main/java/com/birdushenin/newssphere/presentation/NewsWindowFragment.kt
@@ -1,15 +1,20 @@
 package com.birdushenin.newssphere.presentation
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import androidx.fragment.app.activityViewModels
+import com.birdushenin.newssphere.R
 import com.birdushenin.newssphere.data.SavedClass
 import com.birdushenin.newssphere.databinding.FragmentNewsWindowBinding
 import com.bumptech.glide.Glide
@@ -32,6 +37,14 @@ class NewsWindowFragment : Fragment(), FragmentScreen {
 //        (binding.toolbar as? Toolbar)?.let {
 //            (requireActivity() as AppCompatActivity).setSupportActionBar(it)
 //        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window: Window = requireActivity().window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = Color.TRANSPARENT
+        }
+
         sharedViewModel.selectedArticle.observe(viewLifecycleOwner) { article ->
             article?.let {
                 binding.mainText.title = it.title
